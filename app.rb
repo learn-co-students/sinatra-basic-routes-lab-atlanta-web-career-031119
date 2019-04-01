9dba3f10e4df65f1e34f6ab3af3de68457d92161
PrincetonJRose <decoyzpr@gmail.com>
2019-04-01 12:22:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Princeton"
+    end
+    get '/hometown' do
+        "My hometown is Queens, NY"
+    end
+    get '/favorite-song' do
+        "My favorite song is Lost in Thoughts All Alone"
+    end
+
 end
